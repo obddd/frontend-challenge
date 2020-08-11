a0bf832d3d06f1de1988ca07111d0b2f7daf5d3f
Obaid <obaidulhaq20@gmail.com>
2020-08-12 04:02:29 +0500

    added infinite scrolling, spinner and some style changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from "react";
-import Repo from "./components/repo/Repo";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Repo from './components/repo/Repo';
+import './App.css';
 
-import { fetchRepos } from "./api/index";
+import pacman from './assets/ajax-loader.gif'
+
+import { fetchRepos } from './api/index';
 
 const App = () => {
   const [repos, setRepos] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const handleScroll = (event) => {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      setPageNumber((prevPage) => prevPage + 1);
+    }
+  };
 
   useEffect(() => {
     const fetchAPI = async () => {
-      setRepos(await fetchRepos(pageNumber));
-    }
-    fetchAPI()
+      setLoading(true);
+      const newRepos = await fetchRepos(pageNumber);
+      setRepos((prevRepo) => [...prevRepo, ...newRepos]);
+      setLoading(false);
+    };
+    fetchAPI();
   }, [pageNumber]);
 
   return (
     <div className="main-container">
-      {repos.map(repo => (
-        <Repo key={repo.id} data={repo} />
-      ))}
+      <div className="child-container" onScroll={handleScroll}>
+        {repos && repos.map((repo) => <Repo key={repo.id} data={repo} />)}
+      </div>
+      {loading && <img className='spinner' src={pacman} alt='pacman'/>}
     </div>
   );
 };
